d32198c861b8ce25cfac83097f7f5dd9e79bda70
ChiaraCarraro <carrarochiara00@gmail.com>
2025-12-16 12:49:32 +0100

    Changes for Lüneburg Data Collection

--- src/prabat.js
+++ src/prabat.js
@@ -625,30 +625,29 @@ document.addEventListener("DOMContentLoaded", async function () {
         console.error("Error during uploading processing:", err);
       } 
       try {
-      if (responseLog.meta.webcam === "true") {
-        // !responseLog.meta.iOSSafari && 
-        await uploadVideo(responseLog.meta.webcam, responseLog.meta.subjID);
-        await pause(5000);
+        if (responseLog.meta.webcam === "true") {
+          // !responseLog.meta.iOSSafari && 
+          await uploadVideo(responseLog.meta.webcam, responseLog.meta.subjID);
+          await pause(5000);
         }
       } catch (err) {
         console.error("Error during uploading processing:", err);
       } 
-      try {
-        i
-        await downloadData(responseLog.data, responseLog.meta.subjID);
-        await pause(5000);
-      } catch (err) {
-        console.error("Error during uploading processing:", err);
-      } 
-      try {
-      if (responseLog.meta.webcam === "true") {
-        // !responseLog.meta.iOSSafari &&
-        await downloadVideo(responseLog.meta.webcam, responseLog.meta.subjID);
-        await pause(2000);
-      }
-      } catch (err) {
-        console.error("Error during uploading processing:", err);
-      } 
+      // try {
+      //   await downloadData(responseLog.data, responseLog.meta.subjID);
+      //   await pause(5000);
+      // } catch (err) {
+      //   console.error("Error during uploading processing:", err);
+      // } 
+      // try {
+      //   if (responseLog.meta.webcam === "true") {
+      //     // !responseLog.meta.iOSSafari &&
+      //     await downloadVideo(responseLog.meta.webcam, responseLog.meta.subjID);
+      //     await pause(2000);
+      //   }
+      // } catch (err) {
+      //   console.error("Error during uploading processing:", err);
+      // } 
       //window.location.href = `./goodbye.html`;
       overlay.classList.add("hidden");
       window.location.href = `https://devpsy.web.leuphana.de/prabat-consent/goodbye.html`;
